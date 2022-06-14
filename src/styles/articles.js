a67cd8e19ff130e6b805a91e2ba276e20e5ee42c
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-14 14:37:46 +0100

    adding some styling for Articles component

---
+++ src/styles/articles.js
@@ -0,0 +1,24 @@
+const articlesStyles = {
+  articleContainer: {
+    margin: "1rem 0",
+    padding: "10px",
+    width: "98%",
+    backgroundColor: "rgb(57, 59, 60)",
+  },
+  title: {
+    fontWeight: "bolder",
+    fontStyle: "italic",
+    marginBottom: "0.5rem",
+  },
+  icons: {
+    marginRight: "3px",
+  },
+  containers: {
+    display: "flex",
+  },
+  votesContainer: {
+    marginRight: "30px",
+  },
+};
+
+export default articlesStyles;
