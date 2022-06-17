81cd711958640bf170472e57b5e8d3a5b383ca43
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-17 11:16:33 +0100

    updated import location of index.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import UserContext from "./utils/UserContext";
 
 import Home from "./components/Home";
 import Topic from "./components/Topic";
 import Navbar from "./components/Navbar/Navbar";
+import Article from "./components/Article";
+import Login from "./components/Login";
 
 import { ThemeProvider } from "@mui/private-theming";
 import theme from "./styles/customTheme";
 
 function App() {
+  const [username, setUsername] = useState(null);
+  const [avatar, setAvatar] = useState(null);
+
+  useEffect(() => {
+    setUsername(localStorage.getItem("username"));
+    setAvatar(localStorage.getItem("avatar"));
+  }, []);
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:topic_name" element={<Topic />} />
-        </Routes>
-      </ThemeProvider>
+      <UserContext.Provider
+        value={{ username, avatar, setUsername, setAvatar }}
+      >
+        <ThemeProvider theme={theme}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:topic_name" element={<Topic />} />
+            <Route path="/:topic_name/:article_id" element={<Article />} />
+            <Route
+              path="/login"
+              element={
+                username && avatar ? <Navigate to="/" replace /> : <Login />
+              }
+            />
+          </Routes>
+        </ThemeProvider>
+      </UserContext.Provider>
     </>
   );
 }
