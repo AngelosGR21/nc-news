c798523f00cfe288718dddf2d1c88b05e12e1ad4
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-17 11:25:30 +0100

    updated navbar to show user avatar and logout link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import {useState, useEffect} from "react"
+import {useState, useEffect, useContext} from "react"
 import useScreenSize from "../../utils/useScreenSize";
 import { fetchTopics } from "../../utils/api";
-import { AppBar, Toolbar, IconButton, Link, Typography} from '@mui/material';
+import UserContext from "../../utils/UserContext";
+import { AppBar, Toolbar, IconButton, Link, Typography, Box} from '@mui/material';
 
 import navbarStyles from "../../styles/navbar";
 import "../../styles/hamburger.css"
@@ -11,10 +12,10 @@ import Hamburger from "./Hamburger"
 import DropdownTopics from "./DropdownTopics";
 
 const Navbar = () => {
+    const {avatar, username} = useContext(UserContext);
     const [menuOpen, setMenuOpen] = useState(false);
     const [topics, setTopics] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    
     const screenWidth = useScreenSize();
     
     useEffect(() => {
@@ -24,7 +25,9 @@ const Navbar = () => {
      })
     },[])
 
-
+    const handleLogout = () => {
+        localStorage.clear();
+    }
 
 if(screenWidth <= 800){
     return (
@@ -34,7 +37,13 @@ if(screenWidth <= 800){
                     <Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
                 </IconButton>
             <img src={logo} alt="Logo" />
-            <Link href="/login" sx={navbarStyles.link}>Login</Link>
+            {avatar && username ?
+                <Box sx={{display: "flex", alignItems: "center"}}>
+                <img src={avatar} alt="avatar" style={navbarStyles.navbarAvatar}/>
+                <Link onClick={handleLogout} href="/login" sx={navbarStyles.link}>Logout</Link>
+                </Box> 
+                    : 
+                <Link href="/login" sx={navbarStyles.link}>Login</Link>}
             </Toolbar>
             <DropdownTopics menuOpen={menuOpen} topics={topics} isLoading={isLoading}/>
         </AppBar>
@@ -49,7 +58,13 @@ if(screenWidth <= 800){
                 <img src={logo} alt="Logo"/>
                 <Typography variant="h5" ml={1} sx={navbarStyles.homeLink}>Home</Typography>
             </Link>
-            <Link href="/login" sx={navbarStyles.link}>Login</Link>
+            {avatar && username ?
+                <Box sx={{display: "flex", alignItems: "center"}}>
+                <img src={avatar} alt="avatar" style={navbarStyles.navbarAvatar}/>
+                <Link onClick={handleLogout} href="/login" sx={navbarStyles.link}>Logout</Link>
+                </Box> 
+                    : 
+                <Link href="/login" sx={navbarStyles.link}>Login</Link>}
             </Toolbar>
         </AppBar>
       )
