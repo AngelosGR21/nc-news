5ab55530a334122c8c614e37fdaed5648765a373
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-17 11:28:15 +0100

    renamed file and export name

---
+++ src/styles/mainContainers.js
@@ -0,0 +1,11 @@
+const mainContainers = {
+  display: "grid",
+  gridTemplateColumns: {
+    md: "1fr 3fr",
+  },
+  overflow: "hidden",
+  width: "100vw",
+  height: "100vh",
+};
+
+export default mainContainers;
