6c80081a6ec1bcdfb089c89a665bc4cd027abf88
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-27 20:48:24 +0100

    changing some global styles from js to css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,34 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 
 import Spinner from "./Spinner";
 import Articles from "./Articles"
 import AllTopics from "./AllTopics"
 
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import { fetchArticles } from "../utils/api";
 
-import homeStyles from "../styles/mainContainers.js"
+import "../styles/mainContainer.css";
 
-const Home = () => { 
-    const [articles, setArticles] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    
-    useEffect(() => {
-      fetchArticles().then((res) => {
-        setArticles(res);
-        setIsLoading(false);
-      })
-    },[])
+const Home = () => {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchArticles().then((res) => {
+      setArticles(res);
+      setIsLoading(false);
+    })
+  }, [])
 
 
-    if(isLoading){
-      return <Spinner/>
-    }
-    
+  if (isLoading) {
+    return <Spinner />
+  }
+
   return (
-    <Box sx={homeStyles} component="section">
-    <AllTopics/>
-    <Articles articles={articles}/>
+    <Box className="global-exported-container" component="section">
+      <AllTopics />
+      <Articles articles={articles} />
     </Box>
   )
 }
--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -1,46 +1,46 @@
 import { useParams } from "react-router-dom"
-import {useState, useEffect} from "react" 
-import {fetchArticles} from "../utils/api"
+import { useState, useEffect } from "react"
+import { fetchArticles } from "../utils/api"
 
 import Spinner from "../components/Spinner"
 import Articles from "../components/Articles"
 import AllTopics from "./AllTopics"
 
-import {Box} from "@mui/material"
-import topicStyles from "../styles/mainContainers"
+import { Box } from "@mui/material"
+import "../styles/mainContainer.css";
 
 const Topic = () => {
-    const {topic_name} = useParams();
+    const { topic_name } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [articles, setArticles] = useState([]);
     const [errorMessage, setErrorMessage] = useState(null);
-    
+
     useEffect(() => {
         fetchArticles(topic_name).then((res) => {
-            if(res === 404){
+            if (res === 404) {
                 setErrorMessage(`Topic "${topic_name}" was not found`);
                 setIsLoading(false);
-            }else{
+            } else {
                 setArticles(res);
                 setIsLoading(false);
             }
         })
-    },[])
+    }, [])
 
-if(isLoading){
-    return <Spinner/>
-}
+    if (isLoading) {
+        return <Spinner />
+    }
 
-if(errorMessage){
-    return <h1>{errorMessage}</h1>
-}
+    if (errorMessage) {
+        return <h1>{errorMessage}</h1>
+    }
 
-  return (
-      <Box component="section" sx={topicStyles}>
-        <AllTopics currentTopic={topic_name}/>
-      <Articles articles={articles}/>
-    </Box>
-  )
+    return (
+        <Box component="section" className="global-exported-container">
+            <AllTopics currentTopic={topic_name} />
+            <Articles articles={articles} />
+        </Box>
+    )
 }
 
 export default Topic
