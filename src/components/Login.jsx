e539ac4e1266fccb8c8f5e9909b7f50fbfd7aa3b
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-27 20:59:33 +0100

    moved Login styles from js to css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,19 @@
-import {useState, useEffect, useContext} from "react";
-import {Navigate} from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { Navigate } from "react-router-dom";
 import { fetchUsers } from "../utils/api";
-import {Box, Button, Typography} from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 import UserContext from "../utils/UserContext";
 
 import Spinner from "./Spinner";
 
-import GlobalStyles from "@mui/material/GlobalStyles";
-import loginStyles from "../styles/login";
+import "../styles/login.css";
 
 const Login = () => {
-    const {setAvatar, setUsername} = useContext(UserContext);
-    const [users,setUsers] = useState([]);
-    const [isLoading,setIsLoading] = useState(true);
+    const { setAvatar, setUsername } = useContext(UserContext);
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [redirect, setRedirect] = useState(false);
-    
+
     useEffect(() => {
         fetchUsers().then((res) => {
             setUsers(res);
@@ -28,29 +27,28 @@ const Login = () => {
         setAvatar(user.avatar_url);
         setUsername(user.username);
         setRedirect(true);
-    }   
-
-    if(redirect){
-        return <Navigate to="/" replace/>
     }
 
-    if(isLoading){
-        return <Spinner/>
+    if (redirect) {
+        return <Navigate to="/" replace />
+    }
+
+    if (isLoading) {
+        return <Spinner />
     }
 
 
-  return (
-    <Box component="section" sx={loginStyles.pageStyles}>
-    <GlobalStyles styles={{body: {overflow: "visible"}}}/>
-    {users.map((user, index) => {
-        return <Box key={index} sx={loginStyles.userContainer}>
-            <img src={user.avatar_url} alt={user.name} style={loginStyles.avatar}/>
-            <Typography variant="h4">{user.username}</Typography>
-            <Button color="error" sx={loginStyles.button} onClick={() => handleClick(user)}>Login</Button>
+    return (
+        <Box component="section" className="login-main-container">
+            {users.map((user, index) => {
+                return <Box key={index} className="login-user-container">
+                    <img src={user.avatar_url} alt={user.name} className="login-avatar" />
+                    <Typography variant="h4">{user.username}</Typography>
+                    <Button color="error" className="login-button" onClick={() => handleClick(user)}>Login</Button>
+                </Box>
+            })}
         </Box>
-    })}
-    </Box>
-  )
+    )
 }
 
 export default Login
