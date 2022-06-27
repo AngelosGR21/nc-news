e0748aab45a8678941aec54a14cd3cf9f7fc42f9
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-27 19:56:05 +0100

    moving styling into css from js and removing the js file

--- src/components/Navbar/DropdownTopics.jsx
+++ src/components/Navbar/DropdownTopics.jsx
@@ -1,24 +1,25 @@
-import {useState} from "react"
-import {Box, Link, Typography, IconButton} from "@mui/material"
-import dropdownTopicsStyles from "../../styles/dropdownTopics";
+import { useState } from "react"
+import { Box, Link, Typography, IconButton } from "@mui/material"
 
 import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 
-const DropdownTopics = ({menuOpen, isLoading, topics}) => {
+import "../../styles/dropdownTopics.css"
+
+const DropdownTopics = ({ menuOpen, isLoading, topics }) => {
     const [showTopics, setShowTopics] = useState(false);
-    return <Box sx={menuOpen ? {...dropdownTopicsStyles.menu, ...dropdownTopicsStyles.expandDropdown} : dropdownTopicsStyles.menu}>
-        <Link href="/" variant="h3" sx={dropdownTopicsStyles.dropdownHomeLink}>Home</Link>
-        <Box sx={dropdownTopicsStyles.topicsDropdownContainer}>
-        <Typography variant="h3">Topics</Typography>
-        <IconButton onClick={() => setShowTopics((prev) => !prev)}>
-            <ArrowDropDownCircleIcon fontSize="large" sx={showTopics ? {...dropdownTopicsStyles.arrowDown , ...dropdownTopicsStyles.arrowReversed} : dropdownTopicsStyles.arrowDown}/>
-        </IconButton>
+    return <Box className={menuOpen ? "dropdownTopics-menu dropdownTopics-expanded" : "dropdownTopics-menu"}>
+        <Link href="/" variant="h3" className="dropdownTopics-link">Home</Link>
+        <Box className="dropdownTopics-topics-container">
+            <Typography variant="h3">Topics</Typography>
+            <IconButton onClick={() => setShowTopics((prev) => !prev)}>
+                <ArrowDropDownCircleIcon fontSize="large" className={showTopics ? "dropdownTopics-arrow-down dropdownTopics-arrow-reversed" : "dropdownTopics-arrow-down"} />
+            </IconButton>
         </Box>
-        {showTopics && 
-            <Box sx={dropdownTopicsStyles.topicsContainer}>
-            {isLoading? "..." : topics.map((topic, index) => {
-            return <Link sx={dropdownTopicsStyles.topicLinks} key={index} href={`/${topic.slug}`}>{topic.slug.toUpperCase()}</Link>
-            })}
+        {showTopics &&
+            <Box className="dropdownTopics-all-topics-container" >
+                {isLoading ? "..." : topics.map((topic, index) => {
+                    return <Link className="dropdownTopics-topic-links" key={index} href={`/${topic.slug}`}>{topic.slug.toUpperCase()}</Link>
+                })}
             </Box>
         }
     </Box>
