8ce1e738fe43cae1409e9f53fab2ac8f5dd5bdc6
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-27 19:28:56 +0100

    moving styling into css from js for allTopics component

--- src/components/AllTopics.jsx
+++ src/components/AllTopics.jsx
@@ -1,13 +1,13 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import { fetchTopics } from "../utils/api"
 import useScreenSize from "../utils/useScreenSize"
 import Spinner from "./Spinner";
 
-import {Box, Link, Typography} from "@mui/material"
+import { Box, Link, Typography } from "@mui/material"
 
-import allTopicsStyles from "../styles/allTopics";
+import "../styles/allTopics.css";
 
-const AllTopics = ({currentTopic}) => {
+const AllTopics = ({ currentTopic }) => {
     const [topics, setTopics] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const width = useScreenSize();
@@ -19,25 +19,25 @@ const AllTopics = ({currentTopic}) => {
         })
     }, [])
 
-if(isLoading){
-    return <Box component="section" sx={allTopicsStyles.allTopicsContainer}>
-        <Spinner/>
-    </Box>
-}
+    if (isLoading) {
+        return <Box component="section" className="allTopics-container">
+            <Spinner />
+        </Box>
+    }
 
-if(width > 800){   
-    return (
-      <Box component="section" sx={allTopicsStyles.allTopicsContainer}>
-          <Typography variant="h3">Topics</Typography>
+    if (width > 800) {
+        return (
+            <Box component="section" className="allTopics-container">
+                <Typography variant="h3">Topics</Typography>
 
-          {topics.map((topic, index) => currentTopic === topic.slug ? 
-          <Typography sx={allTopicsStyles.currentTopic} key={index}>{topic.slug}</Typography>  
-          : 
-          <Link href={`/${topic.slug}`} key={index} sx={allTopicsStyles.link}>{topic.slug}</Link>
-    )}
-      </Box>
-    )
-  }
+                {topics.map((topic, index) => currentTopic === topic.slug ?
+                    <Typography className="current-topic" key={index}>{topic.slug}</Typography>
+                    :
+                    <Link href={`/${topic.slug}`} className="sidebar-link" key={index}>{topic.slug}</Link>
+                )}
+            </Box>
+        )
+    }
 }
 
 export default AllTopics
