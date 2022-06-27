bf8e4277aa51efcd64043a39e6ac6b7de52076a5
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-27 20:36:31 +0100

    moving styling from js to css for Articles component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,40 +2,40 @@ import { Box, Typography } from "@mui/material";
 
 import Link from '@mui/material/Link';
 
-import articlesStyles from "../styles/articles"
+import "../styles/articles.css";
 
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import CommentIcon from '@mui/icons-material/Comment';
 
-const Articles = ({articles}) => {
-  return (
-    <Box component="main" sx={articlesStyles.mainContainer}>
-    {articles.map((article) => {
-        const {article_id, title, body, topic, votes, comment_count, author} = article;
+const Articles = ({ articles }) => {
+    return (
+        <Box component="main" className="articles-main-container">
+            {articles.map((article) => {
+                const { article_id, title, body, topic, votes, comment_count, author } = article;
 
-        return <Box key={article_id} component="article" sx={articlesStyles.articleContainer}>
-                <Box sx={articlesStyles.headersContainer}>
-                    <Typography>{topic} - created by: {author}</Typography>
-                    <Link href={`/${topic}/${article_id}`}>Read Article</Link>
-                </Box>
-                <Box component="header">
-                <Typography variant="h5" component="h5" sx={articlesStyles.title}>{title}</Typography>
-                <Typography variant="p" component="p">{body.slice(0, 100)}...</Typography>
-                </Box>
-                <Box component="section" sx={{...articlesStyles.containers, ...articlesStyles.reactionsContainer}}>
-                    <Box sx={{...articlesStyles.containers, ...articlesStyles.votesContainer}} component="div">
-                        <ArrowUpwardIcon sx={articlesStyles.icons}/>
-                        <Typography component="p">{votes}</Typography>
+                return <Box key={article_id} component="article" className="articles-article-container">
+                    <Box className="articles-headers-container">
+                        <Typography>{topic} - created by: {author}</Typography>
+                        <Link href={`/${topic}/${article_id}`}>Read Article</Link>
                     </Box>
-                    <Box sx={{...articlesStyles.containers}} component="div">
-                        <CommentIcon sx={articlesStyles.icons}/>
-                        <Typography component="p">{comment_count}</Typography>
+                    <Box component="header">
+                        <Typography variant="h5" component="h5" className="articles-title">{title}</Typography>
+                        <Typography variant="p" component="p">{body.slice(0, 100)}...</Typography>
+                    </Box>
+                    <Box component="section" className="articles-containers articles-reactions-container">
+                        <Box className="articles-containers articles-votes-container" component="div">
+                            <ArrowUpwardIcon className="articles-icons" />
+                            <Typography component="p">{votes}</Typography>
+                        </Box>
+                        <Box className="articles-containers" component="div">
+                            <CommentIcon className="articles-icons" />
+                            <Typography component="p">{comment_count}</Typography>
+                        </Box>
                     </Box>
                 </Box>
-               </Box>
-    })}
-    </Box>
-  )
+            })}
+        </Box >
+    )
 }
 
 export default Articles
